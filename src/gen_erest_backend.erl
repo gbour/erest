8686146699b668d6163266e633d784848f708f73
Guillaume Bour <guillaume@bour.cc>
2012-08-02 21:23:07 +0200

    [fix] behaviour_info syntax

--- src/gen_erest_backend.erl
+++ src/gen_erest_backend.erl
@@ -3,7 +3,7 @@
 -export([behaviour_info/1]).
 
 behaviour_info(callbacks) ->
-	[{list/2}, {insert/3}, {update/4}, {lookup/3}, {delete/3}];
+	[{list,2}, {insert,3}, {update,4}, {lookup,3}, {delete,3}];
 behaviour_info(_) ->
 	undefined.
 
